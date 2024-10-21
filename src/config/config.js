18def7a18eedec0c54ef0440a0a4fef01a43a2f6
Atanas Lazarovski <lazarovski.atanas@gmail.com>
2024-10-21 21:58:07 +0200

    fix connection string

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,4 @@
 module.exports = {
   port: process.env.PORT || 3000,
-  mongodb_uri:
-    process.env.MONGODB_URI || "mongodb://localhost:27017/hello?replicaSet=rs0",
+  mongodb_uri: process.env.MONGODB_URI || "mongodb://localhost:27017/hello",
 };
